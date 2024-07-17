ed3fdfdc532721600475ff1e0034d2ff85a6dd7a
Ansh Bhargava <anshbhargava06@gmail.com>
2024-07-17 15:51:02 +0530

    changes

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,3 +1,4 @@
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
@@ -5,11 +6,12 @@ const verifyToken = (req, res, next) => {
     if (authHeader) {
         const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-            if (err) return res.status(403).json("Invalid token");
+            if (err) 
+                return res.status(403).json("Invalid token");
             req.user = user;
-            console.log(user);
+            console.log(user)
             next();
-        });
+        })
     } else {
         return res.status(401).json("You are not authenticated");
     }
@@ -17,22 +19,21 @@ const verifyToken = (req, res, next) => {
 
 const verifyAndAuthorization = (req, res, next) => {
     verifyToken(req, res, () => {
-        if (req.user.id === req.params.id || req.user.isAdmin) {
+        if (req.user.id === req.params.id) {
             next();
         } else {
-            res.status(403).json("You are restricted from performing this operation");
+            res.status(403).json("You are restricted for performing this operation");
         }
-    });
-};
+    })
+}
 
 const verifyAndAdmin = (req, res, next) => {
     verifyToken(req, res, () => {
         if (req.user.isAdmin) {
             next();
         } else {
-            res.status(403).json("You are restricted from performing this operation");
+            res.status(403).json("You are restricted for performing this operation");
         }
-    });
-};
-
-module.exports = { verifyToken, verifyAndAuthorization, verifyAndAdmin };
+    })
+}
+module.exports = { verifyToken, verifyAndAuthorization ,verifyAndAdmin};
