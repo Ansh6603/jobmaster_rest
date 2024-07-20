03d6bdd150f2a417c6cc12415884abd477f9755f
Ansh Bhargava <anshbhargava06@gmail.com>
2024-07-21 00:21:37 +0530

    bookmark model update

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -3,6 +3,10 @@ const BookMarkSchema = new mongoose.Schema(
     {
         job: { type: mongoose.Schema.Types.ObjectId, ref:"Job" },
         userId: { type: String, required: true },
+        title: { type: String, required: true },
+        imageUrl: { type: String, required: true },
+        company: { type: String, required: true },
+        location: { type: String, required: true },
     }, { timestamp: true }
 );
 module.exports = mongoose.model("Bookmark", BookMarkSchema)
