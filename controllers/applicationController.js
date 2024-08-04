c4d46eb6fd91a5bdd88f87c3854b692a4caec286
Ansh Bhargava <anshbhargava06@gmail.com>
2024-08-05 01:45:52 +0530

    changes

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -4,17 +4,24 @@ const Job = require('../models/Job');
 module.exports = {
   applyForJob: async (req, res) => {
     const { jobId, resumeUrl } = req.body;
-    const userId = req.user.id; 
+    const userId = req.user.id;
+
     try {
       const job = await Job.findById(jobId);
       if (!job) {
-        return res.status(404).json('Job not found');
+        return res.status(404).json({ message: 'Job not found' });
+      }
+
+      const existingApplication = await Application.findOne({ jobId, userId });
+      if (existingApplication) {
+        return res.status(400).json({ message: 'You have already applied for this job' });
       }
 
       const newApplication = new Application({
         jobId,
         userId,
-        resumeUrl
+        resumeUrl,
+        isApplied: true 
       });
 
       const savedApplication = await newApplication.save();
@@ -24,7 +31,7 @@ module.exports = {
         application: savedApplication
       });
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ message: 'Server error', error: error.message });
     }
   }
 };
--- models/Application.js
+++ models/Application.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const ApplicationSchema = new mongoose.Schema({
   jobId: { type: mongoose.Schema.Types.ObjectId, ref: 'Job', required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  resumeUrl: { type: String } 
+  resumeUrl: { type: String } ,
+  isApplied: { type: Boolean, default: false } 
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('Application', ApplicationSchema);
